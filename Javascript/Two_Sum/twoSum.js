5f995f3629b6eb100c2edeec555779f6e625b79b
Sakib548 <mdsakibhassan548@gmail.com>
2024-03-02 19:02:18 +0600

    Two Sum Javascript

---
+++ Javascript/Two_Sum/twoSum.js
@@ -0,0 +1,13 @@
+let twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+    return null;
+  }
+};
+
+let nums = [2, 7, 11, 15];
+console.log(twoSum(nums, 9));
